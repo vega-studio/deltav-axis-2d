2789d642fba9bb3623f996af19b65f454fec7ee7
Diniden <diniden@outlook.com>
2020-02-17 14:32:06 -0700

    Made axis type available

--- dist/src/types/type.d.ts
+++ dist/src/types/type.d.ts
@@ -1,8 +1,3 @@
-export declare enum AxisDataType {
-    DATE = 0,
-    NUMBER = 1,
-    LABEL = 2
-}
 export declare type Vec2 = [number, number];
 export declare type Vec3 = [number, number, number];
 export declare type Color = [number, number, number, number];
--- src/types/type.ts
+++ src/types/type.ts
@@ -1,9 +1,3 @@
-export enum AxisDataType {
-  DATE,
-  NUMBER,
-  LABEL
-}
-
 export type Vec2 = [number, number];
 export type Vec3 = [number, number, number];
 export type Color = [number, number, number, number];
