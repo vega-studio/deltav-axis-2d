1706694c59a925381c143a38b33c04ff90ec3dca
heryoyo1989 <heryoyo1989@gmail.com>
2020-04-30 16:07:43 -0500

    add export

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,4 +2,5 @@ export * from "./basic-axis-store";
 export * from "./date-axis-store";
 export * from "./label-axis-store";
 export * from "./number-axis-store";
+export * from "./range-number-axis-store";
 export * from "./bucket";
